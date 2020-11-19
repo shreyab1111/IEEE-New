bb9c4ead00d3df7ce146466dc14e7f090c0417bf
Biparnak Roy <roybiparnak@gmail.com>
2020-11-20 01:36:25 +0530

    dark-mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-!(function($) {
+!(function ($) {
     "use strict";
 
     // Smooth scroll for the navigation menu and links with .scrollto classes
     var scrolltoOffset = $('#header').outerHeight() - 1;
-    $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+    $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function (e) {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             if (target.length) {
@@ -35,7 +35,7 @@
     });
 
     // Activate smooth scroll on page load with hash links in the url
-    $(document).ready(function() {
+    $(document).ready(function () {
         if (window.location.hash) {
             var initial_nav = window.location.hash;
             if ($(initial_nav).length) {
@@ -51,10 +51,10 @@
     var nav_sections = $('section');
     var main_nav = $('.nav-menu, .mobile-nav');
 
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         var cur_pos = $(this).scrollTop() + 200;
 
-        nav_sections.each(function() {
+        nav_sections.each(function () {
             var top = $(this).offset().top,
                 bottom = top + $(this).outerHeight();
 
@@ -79,19 +79,19 @@
         $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
         $('body').append('<div class="mobile-nav-overly"></div>');
 
-        $(document).on('click', '.mobile-nav-toggle', function(e) {
+        $(document).on('click', '.mobile-nav-toggle', function (e) {
             $('body').toggleClass('mobile-nav-active');
             $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
             $('.mobile-nav-overly').toggle();
         });
 
-        $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
+        $(document).on('click', '.mobile-nav .drop-down > a', function (e) {
             e.preventDefault();
             $(this).next().slideToggle(300);
             $(this).parent().toggleClass('active');
         });
 
-        $(document).click(function(e) {
+        $(document).click(function (e) {
             var container = $(".mobile-nav, .mobile-nav-toggle");
             if (!container.is(e.target) && container.has(e.target).length === 0) {
                 if ($('body').hasClass('mobile-nav-active')) {
@@ -108,7 +108,7 @@
     }
 
     // Toggle .header-scrolled class to #header when page is scrolled
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             //$('#header').removeClass('header-scrolled1');
             $('#topbar').addClass('topbar-scrolled');
@@ -123,7 +123,7 @@
     });
 
     // Carousel Jumbrotorn image toggle
-    $(document).ready(function() {
+    $(document).ready(function () {
         var width = $(window).width();
         var height = $(window).height();
 
@@ -140,16 +140,16 @@
     // Intro carousel
     var heroCarousel = $("#heroCarousel");
     var heroCarouselIndicators = $("#hero-carousel-indicators");
-    heroCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
+    heroCarousel.find(".carousel-inner").children(".carousel-item").each(function (index) {
         (index === 0) ?
-        heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "' class='active'></li>"):
+            heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "' class='active'></li>") :
             heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "'></li>");
     });
 
 
 
     // Back to top button
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.back-to-top').fadeIn('slow');
         } else {
@@ -162,7 +162,7 @@
         $('#topbar').addClass('topbar-scrolled');
     }
 
-    $('.back-to-top').click(function() {
+    $('.back-to-top').click(function () {
         $('html, body').animate({
             scrollTop: 0
         }, 1500, 'easeInOutExpo');
@@ -170,9 +170,9 @@
     });
 
     // Preloader
-    $(window).on('load', function() {
+    $(window).on('load', function () {
         if ($('#preloader').length) {
-            $('#preloader').delay(100).fadeOut('slow', function() {
+            $('#preloader').delay(100).fadeOut('slow', function () {
                 $(this).remove();
             });
         }
@@ -225,7 +225,7 @@
     });
 
     // Initiate venobox lightbox
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('.venobox').venobox();
     });
 
@@ -236,10 +236,19 @@
             once: true
         });
     }
-    $(window).on('load', function() {
+    $(window).on('load', function () {
         aos_init();
     });
 
+
+    //Night-mode-toggle
+    $(" .icofont-moon").click(function () {
+        $("body").addClass("dark-mode");
+    });
+
+    $(" .icofont-sun-alt").click(function () {
+        $("body").removeClass("dark-mode");
+    });
 
 
 
